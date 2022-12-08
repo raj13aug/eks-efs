ab3f2f60cd926561e2c29b3734912437338e9f99
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-12-08 20:23:10 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -23,35 +23,76 @@ module "vpc" {
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = 1
+    "kubernetes.io/role/elb"                    = 1
   }
-  
+
   private_subnet_tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = 1
-  }  
+    "kubernetes.io/role/internal-elb"           = 1
+  }
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "18.17.0"
+  version = "18.21.0"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
 
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  subnet_ids = module.vpc.public_subnets
 
+  cluster_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      resolve_conflicts = "OVERWRITE"
+    }
+  }
 
   enable_irsa = true
 
-  create_cluster_security_group = false
-  create_node_security_group    = false
+  /* create_cluster_security_group = true
+  create_node_security_group    = true */
 
+  # Extend cluster security group rules
+  cluster_security_group_additional_rules = {
+    egress_nodes_ephemeral_ports_tcp = {
+      description                = "To node 1025-65535"
+      protocol                   = "tcp"
+      from_port                  = 1025
+      to_port                    = 65535
+      type                       = "egress"
+      source_node_security_group = true
+    }
+  }
+
+  # Extend node-to-node security group rules
+  node_security_group_additional_rules = {
+    ingress_self_all = {
+      description = "Node to node all ports/protocols"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+    egress_all = {
+      description      = "Node all egress"
+      protocol         = "-1"
+      from_port        = 0
+      to_port          = 0
+      type             = "egress"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
+  }
 
   eks_managed_node_groups = {
     initial = {
-      instance_types = ["t3.medium"]
+      instance_types                        = ["t3.medium"]
       create_security_group                 = false
       attach_cluster_primary_security_group = true
 
@@ -64,6 +105,8 @@ module "eks" {
       ]
     }
   }
+  #  List of CIDR blocks which can access the Amazon EKS public API server endpoint
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
 
   tags = {
     "karpenter.sh/discovery" = var.cluster_name
