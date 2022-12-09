2106978dbbde54e992daf43978d9e74119d50250
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-12-09 12:09:04 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-provider "kubectl" {
+/* provider "kubectl" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
@@ -21,4 +21,4 @@ provider "helm" {
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.eks-cluster.token
   }
-}
+} */
