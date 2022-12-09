0f3cb7b07735293c0a1681cb902ff603d4638e9c
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-12-09 17:29:55 +0530

    updated

--- efs.tf
+++ efs.tf
@@ -190,25 +190,46 @@ resource "helm_release" "efs_csi_driver" {
 
 }
 
-resource "kubernetes_storage_class_v1" "efs_storage_class" {
-
+resource "kubernetes_storage_class_v1" "efs_sc" {
   metadata {
     name = "efs-sc"
   }
   storage_provisioner = "efs.csi.aws.com"
-  reclaim_policy      = "Delete"
-  volume_binding_mode = "Immediate"
-  parameters = {
-    "provisioningMode" = "efs-ap"
-    "fileSystemId"     = aws_efs_file_system.efs.id
-    "directoryPerms"   = "755"
-    "uid"              = "1000"
-    "gid"              = "1000"
-    "basePath"         = "/dynamic_provisioning"
+}
+
+resource "kubernetes_persistent_volume_v1" "efs_pv" {
+  metadata {
+    name = "efs-pv"
   }
+  spec {
+    capacity = {
+      storage = "5Gi"
+    }
+    volume_mode                      = "Filesystem"
+    access_modes                     = ["ReadWriteMany"]
+    persistent_volume_reclaim_policy = "Retain"
+    storage_class_name               = kubernetes_storage_class_v1.efs_sc.metadata[0].name
+    persistent_volume_source {
+      csi {
+        driver        = "efs.csi.aws.com"
+        volume_handle = aws_efs_file_system.efs.id
+      }
+    }
+  }
+}
 
-  depends_on = [
-    helm_release.efs_csi_driver
-  ]
 
+resource "kubernetes_persistent_volume_claim_v1" "efs_pvc" {
+  metadata {
+    name = "efs-claim"
+  }
+  spec {
+    access_modes       = ["ReadWriteMany"]
+    storage_class_name = kubernetes_storage_class_v1.efs_sc.metadata[0].name
+    resources {
+      requests = {
+        storage = "5Gi"
+      }
+    }
+  }
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,11 @@
 Inside container check mount points.
 
-#kubectl exec -ti efs-app -- bash
+kubectl apply -f efs-writer.yaml
+kubectl apply -f efs-reader.yaml
+
+
+kubectl exec -it efs-writer -- tail /shared/out.txt
+kubectl exec -it efs-reader -- tail /shared/out.txt
 
 
 Reference url :
@@ -8,3 +13,8 @@ Reference url :
 https://computingforgeeks.com/eks-persistent-storage-with-efs-aws-service/
 
 https://docs.aws.amazon.com/eks/latest/userguide/sec-group-reqs.html
+
+
+
+kubectl apply -f efs-writer.yaml
+kubectl apply -f efs-reader.yaml
